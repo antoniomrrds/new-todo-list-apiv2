7ef7ebe81a184fdad4bce7f2016fd5d88d603594
antoniomrrds <arqueiroverd@gmail.com>
2024-10-07 04:02:17 -0300

    Add application layer about Dto , Validator and mapper reference a tag

---
+++ src/Core/TodoList.Application/Mappings/TagProfile.cs
@@ -0,0 +1,12 @@
+﻿using TodoList.Application.DTOs;
+using TodoList.Domain.Entities;
+
+namespace TodoList.Application.Mappings;
+
+public class TagProfile: TodoProfile
+{
+    public TagProfile()
+    {
+        CreateMap<TagDTo, Tag>();
+    }
+}
--- src/dump/init.sql
+++ src/dump/init.sql
@@ -4,15 +4,19 @@ USE db_todo_list;
 
 CREATE TABLE IF NOT EXISTS tbl_tag
 (
-    ID_Tag     SMALLINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    ID     SMALLINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
     Name       VARCHAR(255) NOT NULL,
+    Slug      VARCHAR(255) NOT NULL,
+    Description   VARCHAR(500) DEFAULT NULL,
+    Status TINYINT(1) DEFAULT 1, -- Status da tag (ativa/inativa),
+    Color         VARCHAR(7) DEFAULT '#FFFFFF',
     Created_At DATETIME     NOT NULL,
     Updated_At DATETIME     NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS tbl_category
 (
-    ID_Category SMALLINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    ID SMALLINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
     Name        VARCHAR(255) NOT NULL,
     Created_At  DATETIME     NOT NULL,
     Updated_At  DATETIME     NOT NULL
@@ -20,18 +24,19 @@ CREATE TABLE IF NOT EXISTS tbl_category
 
 CREATE TABLE IF NOT EXISTS tbl_todo
 (
-    ID_Todo      INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    ID      INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
     Title        VARCHAR(255)      NOT NULL,
     Description  TEXT              NOT NULL,
     Is_Completed BOOLEAN           NOT NULL,
     Created_At   DATETIME          NOT NULL,
+    Status TINYINT(1) DEFAULT 1, -- Status da tag (ativa/inativa),
     Updated_At   DATETIME          NOT NULL,
     ID_Tag       SMALLINT UNSIGNED NULL, 
     ID_Category  SMALLINT UNSIGNED NULL, 
-    CONSTRAINT fk_id_tag FOREIGN KEY (ID_Tag) REFERENCES tbl_tag (ID_Tag)
+    CONSTRAINT fk_id_tag FOREIGN KEY (ID_Tag) REFERENCES tbl_tag (ID)
         ON DELETE SET NULL
         ON UPDATE CASCADE,
-    CONSTRAINT fk_id_Category FOREIGN KEY (ID_Category) REFERENCES tbl_category (ID_Category)
+    CONSTRAINT fk_id_Category FOREIGN KEY (ID_Category) REFERENCES tbl_category (ID)
         ON DELETE SET NULL
         ON UPDATE CASCADE
 );
